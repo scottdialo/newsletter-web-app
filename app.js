d4f64ee642649492a917eef9f1f30510f4c8f9b7
Scott Dialo <scottdialo@Scotts-MacBook-Pro.local>
2022-12-08 11:31:28 -0800

    env file add

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(express.static("public")); //this serve static files
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/signup.html");
+  console.log(process.env.SECRET_KEY);
 });
 
 // failure post route
@@ -44,7 +45,7 @@ app.post("/", function (req, res) {
   const url = "https://us14.api.mailchimp.com/3.0/lists/fcec82a20f"; //endpoint to add subsriber to our mailchip account
   const options = {
     method: "POST",
-    auth: process.env.SECRET_KEY, //authentication, anyting as usrNmae and api key as pwrd
+    auth: "apiKey:" + process.env.SECRET_KEY, //authentication, anyting as usrNmae and api key as pwrd
   };
 
   const requestData = https.request(url, options, function (response) {
